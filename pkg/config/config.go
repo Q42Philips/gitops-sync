b4129ec71ba570b35594ffd3e24ffa9590328f9a
Herman Banken <hermanbanken@gmail.com>
2021-05-28 16:07:48 +0200

    move auth

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,17 +1,12 @@
 package config
 
 import (
-	"errors"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"time"
 
 	"github.com/jnovack/flag"
-
-	githttp "github.com/go-git/go-git/v5/plumbing/transport/http"
-	"github.com/google/go-github/v33/github"
 )
 
 var Global Config
@@ -93,47 +88,5 @@ func (c *Config) ParseAndValidate() {
 			refName = "unknown"
 		}
 		Global.CommitMsg = fmt.Sprintf("Sync %s/%s", project, refName)
-	}
-}
-
-func (c *Config) GetClientAuth() (hubClient *github.Client, gitAuth githttp.AuthMethod, err error) {
-	if Global.AuthUsername != "" {
-		hubAuth := &github.BasicAuthTransport{Username: c.AuthUsername, Password: c.AuthPassword, OTP: c.AuthOtp}
-		hubClient = github.NewClient(hubAuth.Client())
-		gitAuth = &BasicAuthWrapper{hubAuth}
-	} else if c.AuthToken != "" {
-		hubAuth := &github.BasicAuthTransport{Username: "x-access-token", Password: c.AuthToken}
-		hubClient = github.NewClient(hubAuth.Client())
-		gitAuth = &BasicAuthWrapper{hubAuth}
-	} else {
-		return nil, nil, errors.New("no authentication provided, see help for authentication options")
-	}
-	log.Println(gitAuth.String())
-	return hubClient, gitAuth, nil
-}
-
-var _ githttp.AuthMethod = &BasicAuthWrapper{}
-
-type BasicAuthWrapper struct {
-	*github.BasicAuthTransport
-}
-
-func (b *BasicAuthWrapper) Name() string {
-	return "http-basic-auth"
-}
-func (b *BasicAuthWrapper) String() string {
-	masked := "*******"
-	if b.Password == "" {
-		masked = "<empty>"
-	}
-	return fmt.Sprintf("%s - %s:%s", b.Name(), b.Username, masked)
-}
-func (b *BasicAuthWrapper) SetAuth(r *http.Request) {
-	if b == nil {
-		return
-	}
-	r.SetBasicAuth(b.Username, b.Password)
-	if b.OTP != "" {
-		r.Header.Set("X-GitHub-OTP", b.OTP)
 	}
 }
